8e0411f0c10c146ab5a3171a784ce0d4fc58ee77
jinjin7766 <jinjin7766@naver.com>
2024-10-30 13:47:37 +0900

    Update app9.py

--- app9.py
+++ app9.py
@@ -50,6 +50,10 @@ def get_response(user_input):
 
 # Streamlit 인터페이스
 st.title("포트폴리오 챗봇")
+
+# 이미지 표시
+st.image("hand.png", caption="Welcome to the Restaurant Chatbot", use_column_width=True)
+
 st.write("포트폴리오에 관한 질문을 입력해보세요. 예: 포트폴리오 주제가 뭔가요?")
 
 user_input = st.text_input("user", "")
